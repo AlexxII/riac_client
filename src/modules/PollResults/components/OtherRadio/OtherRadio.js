7eaabd44f6065991fb4454757ed707b2938076c0
AlexxII <murmus51@yandex.ru>
2021-01-30 19:53:32 +0300

    radio update

--- src/modules/PollResults/components/OtherRadio/OtherRadio.js
+++ src/modules/PollResults/components/OtherRadio/OtherRadio.js
@@ -2,7 +2,9 @@ import React from "react";
 
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import { Fragment } from "react";
 
 const OtherRadio = ({ answer, onChange, settings }) => {
 
@@ -14,6 +16,35 @@ const OtherRadio = ({ answer, onChange, settings }) => {
 
   }
 
+  const AnswerTitle = () => {
+    if (settings.codesShow) {
+      return (
+        <Fragment>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <span className={"answer-code"}>{answer.code} - </span>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                id="standard-bare"
+                value={answer.text}
+                placeholder={answer.title}
+                margin="normal"
+                onChange={onTextChange}
+                className="radio-control-label"
+              />
+            </Grid>
+          </Grid>
+
+        </Fragment>
+      )
+    } else {
+      return (
+        answer.title
+      )
+    }
+  }
+
   return (
     <FormControlLabel
       value={answer.id}
@@ -21,15 +52,14 @@ const OtherRadio = ({ answer, onChange, settings }) => {
       control={<Radio />}
       className="other-radio-label"
       label={
-        <TextField
-          id="standard-bare"
-          value={answer.text}
-          placeholder={answer.title}
-          margin="normal"
-          onChange={onTextChange}
-          className="radio-control-label"
-        />
+        <AnswerTitle />
       }
+      // label={
+      //   <Fragment>
+      //     <span className={"answer-code"}>{answer.code} - </span>
+
+      //   </Fragment>
+      // }
       disabled={answer.disabled}
     />
   )

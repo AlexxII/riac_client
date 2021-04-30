b8608005044eae1db2d0e07a8a5e6397484ca471
AlexxII <murmus51@yandex.ru>
2021-04-30 16:08:19 +0300

    middleware commit

--- src/modules/PollResults/containers/Analytics/Analytics.js
+++ src/modules/PollResults/containers/Analytics/Analytics.js
@@ -8,6 +8,16 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 import Pagination from '@material-ui/lab/Pagination';
+import Tooltip from '@material-ui/core/Tooltip';
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import Checkbox from '@material-ui/core/Checkbox';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import IconButton from '@material-ui/core/IconButton';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
 import LoadingState from '../../../../components/LoadingState'
 import ErrorState from '../../../../components/ErrorState'
@@ -43,7 +53,31 @@ const Analytics = ({ id }) => {
   const [prevSimQuestions, setPrevSimQuestions] = useState(null)
   const [simQuestions, setSimQuestions] = useState(false)
 
-  const [getSameQuestions, { loading: ll, data: dd }] = useLazyQuery(GET_QUESTIONS_WITH_SAME_TOPICS);
+  const [getSameQuestions, { loading: ll, data: dd }] = useLazyQuery(GET_QUESTIONS_WITH_SAME_TOPICS, {
+    onError: ({ graphQLErrors }) => {
+      let message = {}
+      for (let err of graphQLErrors) {
+        switch (err.extensions.code) {
+          case 'BAD_USER_INPUT':
+            if (err.extensions.type === '00043') {
+              message = {
+                type: 'error',
+                text: 'Опрос с таким кодом уже существует'
+              }
+            }
+            break
+          default:
+            message = {
+              type: 'error',
+              text: 'Добавить не удалось. Смотрите консоль.'
+            }
+        }
+      }
+      setNoti(message)
+      console.log(graphQLErrors);
+      setProcessing(false)
+    }
+    });
 
   useEffect(() => {
     if (dd && !ll) {
@@ -52,7 +86,7 @@ const Analytics = ({ id }) => {
       const upQuestions = sortQuestionsBySimilarity(questions, mainTitle)
       setPrevSimQuestions(upQuestions)
       setSimQuestions(upQuestions)
-      console.log(upQuestions);
+      setProcessing(false)
     }
   }, [dd, ll])
 
@@ -109,6 +143,7 @@ const Analytics = ({ id }) => {
   }
 
   const handleClick = (topicId) => {
+    setProcessing(true)
     if (previousTopicId && previousTopicId === topicId) {
       const mainText = pollData.poll.questions[resultCount - 1].title
       const sortedQuestions = sortQuestionsBySimilarity(prevSimQuestions, mainText)
@@ -144,38 +179,97 @@ const Analytics = ({ id }) => {
       <Divider />
       <div className="info-zone">
         <Typography variant="body2" gutterBottom>
-          Позволяет проанализирвать и задать распределение на основе аналогичных опросов.
+          Позволяет анализировать и задавать частотное распределение ответов на основе банка опросов.
         </Typography>
       </div>
       <br></br>
       <Grid>
         {pollData &&
           <Fragment>
-            <Pagination
-              count={pollData.poll.questions.length}
-              page={resultCount}
-              variant="outlined"
-              color="primary"
-              shape="rounded"
-              onChange={handleResultChange}
-              boundaryCount={10}
-            />
-            <p>
-              {
-                pollData.poll.questions[resultCount - 1].title
-              }
-            </p>
-            <p>{
-              pollData.poll.questions[resultCount - 1].topic.title
-            }</p>
-            <Button onClick={() => handleClick(pollData.poll.questions[resultCount].topic.id)}>Забрать</Button>
-            <p>
-              {simQuestions &&
-                simQuestions.map((question) => (
-                  <p onClick={() => console.log(question)} key={uuid()}>{question.title}  - {question.topic.title} - {question.poll.code}</p>
-                ))
-              }
-            </p>
+            <div className="pagination-wrap">
+              <span className="pagination-wrap-title">
+                <strong>Вопросы:</strong>
+              </span>
+              <Pagination
+                count={pollData.poll.questions.length}
+                page={resultCount}
+                variant="outlined"
+                color="primary"
+                shape="rounded"
+                onChange={handleResultChange}
+                boundaryCount={10}
+              />
+            </div>
+            <div className="analitics-main-content">
+              <List dense={true} className="analitics-main-question">
+                <ListItem>
+                  <ListItemText
+                    primary={
+                      <Fragment>
+                        <strong>Вопрос: </strong>
+                        {pollData.poll.questions[resultCount - 1].title}
+                      </Fragment>
+                    }
+                    secondary={
+                      <Fragment>
+                        <strong>Тема: </strong>
+                        {`ID: ${pollData.poll.questions[resultCount - 1].topic.id} - ${pollData.poll.questions[resultCount - 1].topic.title}`}
+                      </Fragment>
+                    }
+                  />
+                  <ListItemSecondaryAction>
+                    <Tooltip title="Запросить аналоги из БД">
+                      <IconButton edge="end" onClick={() => handleClick(pollData.poll.questions[resultCount - 1].topic.id)}>
+                        <AccountTreeIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </ListItemSecondaryAction>
+
+                </ListItem>
+              </List>
+              <p>
+                <List dense={true} className="analitics-sim-question">
+                  {simQuestions &&
+                    simQuestions.map((question) => (
+                      <Fragment>
+                      <ListItem>
+                        <ListItemIcon>
+                          <Checkbox
+                            edge="start"
+                            checked={true}
+                            tabIndex={-1}
+                            disableRipple
+                          />
+                          </ListItemIcon>
+                        <ListItemText
+                            primary={
+                            <Fragment>
+                              <strong>Вопрос: </strong>
+                              {question.title}
+                            </Fragment>
+                          }
+                          secondary={
+                            <Fragment>
+                              <strong>Тема: </strong>
+                              {`ID: ${question.topic.id} - ${question.topic.title}`}
+                            </Fragment>
+                          }
+                        />
+                        <ListItemSecondaryAction>
+                          <Tooltip title="Просмотреть ответы">
+                            <IconButton edge="end" onClick={() => console.log('1111111111')}>
+                              <AccountTreeIcon />
+                            </IconButton>
+                          </Tooltip>
+                        </ListItemSecondaryAction>
+
+                      </ListItem>
+                        <Divider variant="inset"/>
+                      </Fragment>
+                    ))}
+                </List>
+              </p>
+            </div>
           </Fragment>
         }
       </Grid>

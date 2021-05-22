14f881b8513d9095c25e2f708e34166a06910618
AlexxII <murmus51@yandex.ru>
2021-05-22 22:12:04 +0300

    drivein settings update

--- src/modules/PollDrive/components/DriveSettingsDialog/DriveSettingsDialog.js
+++ src/modules/PollDrive/components/DriveSettingsDialog/DriveSettingsDialog.js
@@ -7,6 +7,8 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
 
 const DriveSettingsDialog = ({ open, handleClose, save, cities, users, currentUser }) => {
   const [user, setUser] = useState(currentUser.id)
@@ -18,7 +20,7 @@ const DriveSettingsDialog = ({ open, handleClose, save, cities, users, currentUs
     const { value } = e.target
     setUser(value)
   }
-  
+
   const handleCityChange = (e) => {
     const { value } = e.target
     setCity(value)
@@ -40,39 +42,48 @@ const DriveSettingsDialog = ({ open, handleClose, save, cities, users, currentUs
       >
         <DialogContent>
           <DialogContentText>
-            Укажите пожалуйста населенный пункт в котором проводился опрос и пользователя который его проводил
+            Укажите пожалуйста населенный пункт в котором проводился опрос и пользователя, который его проводил
           </DialogContentText>
-          <InputLabel id="cities-label" style={{ color: 'black' }}>Населенный пункт:</InputLabel>
-          <Select
-            fullWidth
-            native
-            id="cities-label"
-            value={city?.id}
-            onChange={e => handleCityChange(e)}
-          >
-            <option aria-label="None" disabled selected value="" />
-            {cities.map((option, i) =>
-              <option key={option.id} value={option.id}>{option.title}</option>
-            )}
-          </Select>
-          <FormHelperText>Выберите населенный пункт</FormHelperText>
+          <FormControl fullWidth>
+            <InputLabel shrink={true} id="cities-label" >Населенный пункт</InputLabel>
+            <Select
+              labelId="cities-label"
+              fullWidth
+              native
+              label="Населенный пункт"
+              id="cities-label"
+              value={city?.id}
+              onChange={e => handleCityChange(e)}
+            >
+              <option aria-label="None" disabled selected value="" />
+              {cities.map((option, i) =>
+                <option key={option.id} value={option.id}>{option.title}</option>
+              )}
+            </Select>
+            <FormHelperText>Выберите населенный пункт</FormHelperText>
+          </FormControl>
           <p></p>
-          <InputLabel id="users-label" style={{ color: 'black' }}>Пользователь:</InputLabel>
-          <Select
-            fullWidth
-            native
-            id="users-label"
-            value={user.id}
-            onChange={e => handleUserChange(e)}
-          >
-            <option aria-label="None" disabled selected value="" />
-            {users.map((option, i) => {
-
-              return (<option key={option.id} value={option.id} selected={option.id === currentUser.id}>{option.username}</option>)
-            }
-            )}
-          </Select>
-          <FormHelperText>Выберите пользователя</FormHelperText>
+          <FormControl fullWidth>
+            <InputLabel id="users-label" style={{ color: 'black' }}>Пользователь</InputLabel>
+            <Select
+              fullWidth
+              native
+              id="users-label"
+              value={user.id}
+              onChange={e => handleUserChange(e)}
+            >
+              <option aria-label="None" disabled selected value="" />
+              {users.map((option, i) =>
+                <option key={option.id} value={option.id} selected={option.id === currentUser.id}>
+                  {option.username}
+                </option>
+              )}
+            </Select>
+            <FormHelperText>Выберите пользователя</FormHelperText>
+          </FormControl>
+          <Typography variant="overline" display="block" gutterBottom>
+            {`Вы: ${currentUser.username}`}
+          </Typography>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">

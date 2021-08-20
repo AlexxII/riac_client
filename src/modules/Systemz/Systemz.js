33b99a34b755ef03b531fd5b2f753a82975611ba
AlexxII <MainAdmin@local.su>
2021-08-20 14:20:17 +0300

    'update'

--- src/modules/Systemz/Systemz.js
+++ src/modules/Systemz/Systemz.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Button from '@material-ui/core/Button';
 
 const Systemz = () => {
-  return(
-    'ку'
+  const handleRawInput = (e) => {
+    e.preventDefault()
+    if (!e.target.files[0]) return
+    let reader = new FileReader();
+    let file = e.target.files[0];
+    if (file) {
+      reader.onloadend = () => {
+        const fileData = reader.result
+        console.log(fileData)
+      }
+    }
+    reader.readAsText(file, 'cp1251');
+    e.target.value = ""
+  }
+
+
+  return (
+    <Fragment>
+      <input
+        accept="*.opr"
+        id="contained-button-file"
+        onInput={handleRawInput}
+        type="file"
+      />
+      <label htmlFor="contained-button-file">
+        <Button
+          variant="contained"
+          color="primary"
+          component="span"
+          size="small"
+        >
+          Выбрать
+        </Button>
+      </label>
+    </Fragment>
   )
 }
 
